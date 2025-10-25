4b26a54c99c800414260200256f7592b29e9e93c
Sirhid24k <abdulateefsaheed7@gmail.com>
2025-10-25 13:55:46 +0100

    Update promises.js

--- promises.js
+++ promises.js
@@ -1,3 +1,119 @@
+// Destructuring
+// Array destructuring
+const studentsLogs = [
+  {
+    name: 'Mark Johnson',
+    age: 25,
+    department: 'Physical Science',
+    clubs: ['art', 'music', 'sports'],
+    courses: {
+      required: ['math', 'english', 'physics'],
+      elective: ['biology'],
+    },
+    checkedIn: true,
+  },
+  {
+    name: 'Jessica Matthews',
+    age: 23,
+    department: 'Social Science',
+    clubs: ['art', 'music'],
+    courses: {
+      required: ['english', 'sociology'],
+      elective: ['math'],
+    },
+    checkedIn: false,
+  },
+  {
+    name: 'Steven Carlson',
+    age: 26,
+    department: 'Electrical Engineering',
+    clubs: ['science', 'hackathon'],
+    courses: {
+      required: ['math', 'english', 'physics'],
+      elective: ['chemistry'],
+    },
+    checkedIn: true,
+  },
+  {
+    name: 'Annastasia Bullhorn',
+    age: 24,
+    department: 'Medicine',
+    clubs: ['art', 'music', 'sports'],
+    courses: {
+      required: ['biology', 'chemistry', 'english'],
+      elective: ['math'],
+    },
+    checkedIn: true,
+  },
+];
+
+const [student1, student2, student3, student4] = studentsLogs;
+console.log(student1.name);
+console.log(student2.age);
+console.log(student3.clubs);
+console.log(student4.courses.required);
+
+// More Array Destructuring
+const fruits = ['orange', 'apple', 'banana', 'pineapple', 'watermelon'];
+
+// Rest Operator
+const [fruit1, fruit2, ...restFruit] = fruits;
+
+// Spread Operator
+const moreFruit = ['grape', ...fruits];
+console.log(fruit1, fruit2);
+console.log(restFruit);
+console.log(moreFruit);
+
+const mark = {
+  firstName: 'Mark Johnson',
+  age: 25,
+  department: 'Physical Science',
+  clubs: ['art', 'music', 'sports'],
+  courses: {
+    required: ['math', 'english', 'physics'],
+    elective: ['biology'],
+  },
+  checkedIn: true,
+};
+
+// Object Destructuring
+// const { firstName, age, department } = mark;
+const { firstName: justName, age: justAge, department: justDepartment } = mark;
+
+// Rest and Spread Operator
+const { firstName, age, department, ...restOfFields } = mark;
+const moreFields = {
+  ...mark,
+  checkedOut: false,
+};
+// console.log(firstName, age, department);
+console.log(justName, justAge, justDepartment);
+console.log(firstName, age, department, restOfFields);
+// console.log(moreFields);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Arrow Functions
+
+const firstNumber = 20;
+const secondNumber = 10;
+let varyingNumber;
+
+// Implicit Function Return
+const addNumbers = (firstNum, secondNum) => firstNum + secondNum;
+
+// Explicit Function Return
+const randomNumber = num => {
+  varyingNumber = Math.random();
+  return varyingNumber * num;
+};
+
+const result = addNumbers(7, 3);
+const secondResult = randomNumber(3);
+console.log(result, secondNumber);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Promises
 // Fetching Promise (Data)
 function fetchData() {
   return new Promise((resolve, reject) => {
@@ -5,6 +121,7 @@ function fetchData() {
 
     // Simulating an API call
     setTimeout(() => {
+      // Test success = false;
       const success = true;
       if (success) {
         resolve('Data fetched successfully!'); // Fulfills the Promise
@@ -18,8 +135,8 @@ function fetchData() {
 // Using the Promise
 fetchData()
   .then(response => {
-    console.log(response); // 'Data fetched successfully!'
+    console.log(response);
   })
   .catch(error => {
-    console.error(error); // 'Error: Could not fetch data.'
+    console.error(error);
   });
